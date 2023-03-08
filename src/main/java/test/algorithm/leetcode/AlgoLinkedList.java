2fc680d671215d70bc60b33364411a86416faf26
adream <listentonatural@gmail.com>
2023-03-08 10:05:19 +0800

    msg: test

--- src/main/java/test/algorithm/leetcode/AlgoLinkedList.java
+++ src/main/java/test/algorithm/leetcode/AlgoLinkedList.java
@@ -17,6 +17,9 @@ public class AlgoLinkedList {
         log.info("SwapNodesInPairs: {}", new SwapNodesInPairs().swapPairs(SwapNodesInPairs.example1()));
         log.info("SwapNodesInPairs: {}", new SwapNodesInPairs().swapPairs(SwapNodesInPairs.example2()));
         log.info("SwapNodesInPairs: {}", new SwapNodesInPairs().swapPairs(SwapNodesInPairs.example3()));
+        log.info("ReverseNodesInKGroup: {}", new ReverseNodesInKGroup().reverseKGroup(ReverseNodesInKGroup.example1(), 2));
+        log.info("ReverseNodesInKGroup: {}", new ReverseNodesInKGroup().reverseKGroup(ReverseNodesInKGroup.example1(), 3));
+        log.info("ReverseNodesInKGroup: {}", new ReverseNodesInKGroup().reverseKGroup(ReverseNodesInKGroup.example2(), 2));
     }
 }
 
@@ -152,7 +155,7 @@ class MergeTwoSortedLists {
  */
 // @lc code=start
 class SwapNodesInPairs {
-    // 解法2
+    // 解法1
     public ListNode swapPairs(ListNode head) {
         if (head == null || head.next == null) return head;
         ListNode dummy = new ListNode(-1);
@@ -188,6 +191,7 @@ class SwapNodesInPairs {
         ListNode second = head.next;
         ListNode others = head.next.next;
         // 先把前两个元素翻转
+        // 此处会导致环异常
         second.next = first;
         // 利用递归定义，将剩下的链表节点两两翻转，接到后面
         first.next = swapPairs(others);
@@ -219,6 +223,101 @@ class SwapNodesInPairs {
     }
     public static ListNode example3() {
         return new ListNode(1);
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=25 lang=java
+ *
+ * [25] K 个一组翻转链表
+ *
+ * https://leetcode.cn/problems/reverse-nodes-in-k-group/description/
+ *
+ * algorithms
+ * Hard (67.75%)
+ * Likes:    1940
+ * Dislikes: 0
+ * Total Accepted:    439.6K
+ * Total Submissions: 649K
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * 给你链表的头节点 head ，每 k 个节点一组进行翻转，请你返回修改后的链表。
+ * k 是一个正整数，它的值小于或等于链表的长度。如果节点总数不是 k 的整数倍，那么请将最后剩余的节点保持原有顺序。
+ * 你不能只是单纯的改变节点内部的值，而是需要实际进行节点交换。
+ *
+ * 示例 1：
+ * 输入：head = [1,2,3,4,5], k = 2
+ * 输出：[2,1,4,3,5]
+ *
+ * 示例 2：
+ * 输入：head = [1,2,3,4,5], k = 3
+ * 输出：[3,2,1,4,5]
+ *
+ * 提示：
+ * 链表中的节点数目为 n
+ * 1 <= k <= n <= 5000
+ * 0 <= Node.val <= 1000
+ *
+ * 进阶：你可以设计一个只用 O(1) 额外内存空间的算法解决此问题吗？
+ */
+// @lc code=start
+class ReverseNodesInKGroup {
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k <= 1) return head;
+        ListNode first = head;
+        ListNode other = first.next;
+        // 遍历剩余节点是否足够 k 个, 并找出下一段头结点
+        for (int i = 1; i < k; i++) {
+            if (other == null) {
+                return first;
+            }
+            other = other.next;
+        }
+
+        // 遍历并翻转 k 个节点
+        ListNode p1 = null;
+        ListNode p2 = first;
+        ListNode p3 = first.next;
+        for (int i = 0; i < k; i++) {
+            p2.next = p1;
+            // 后移前 2 个指针
+            p1 = p2;
+            p2 = p3;
+            // 后移第 3 个指针时判断是否为空
+            if (p3 == null) {
+                break;
+            }
+            p3 = p3.next;
+        }
+        first.next = reverseKGroup(other, k);
+        return p1;
+    }
+
+
+    /**
+     * Definition for singly-linked list.
+     */
+    private static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        @Override
+        public String toString() {
+            return val + ", " + next;
+        }
+    }
+
+    public static ListNode example1() {
+        return new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    }
+
+    public static ListNode example2() {
+        return new ListNode(1, new ListNode(2));
     }
 }
 // @lc code=end
