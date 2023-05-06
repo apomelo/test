915b6ea603cb0e3cd6dcb0ea26c8677dfc2d7f18
adream <listentonatural@gmail.com>
2023-05-06 09:38:09 +0800

    msg: algo array

--- src/main/java/test/algorithm/leetcode/AlgoArray.java
+++ src/main/java/test/algorithm/leetcode/AlgoArray.java
@@ -2,7 +2,8 @@ package test.algorithm.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 数组
@@ -25,6 +26,16 @@ public class AlgoArray {
         int[] nextPermutationExample4 = new int[] {1,4,3,2};
         new NextPermutation().nextPermutation(nextPermutationExample4);
         log.info("NextPermutation: {}", nextPermutationExample4);
+        // 旋转图像
+        int[][] rotateImageExample1 = new int[][] {{1,2,3},{4,5,6},{7,8,9}};
+        new RotateImage().rotate(rotateImageExample1);
+        log.info("RotateImage: {}", (Object) rotateImageExample1);
+        int[][] rotateImageExample2 = new int[][] {{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+        new RotateImage().rotate(rotateImageExample2);
+        log.info("RotateImage: {}", (Object) rotateImageExample2);
+        // 螺旋矩阵
+        log.info("SpiralMatrix: {}", new SpiralMatrix().spiralOrder(new int[][] {{1,2,3},{4,5,6},{7,8,9}}));
+        log.info("SpiralMatrix: {}", new SpiralMatrix().spiralOrder(new int[][] {{1,2,3,4},{5,6,7,8},{9,10,11,12}}));
     }
 }
 
@@ -118,3 +129,163 @@ class NextPermutation {
     }
 }
 // @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=48 lang=java
+ *
+ * [48] 旋转图像
+ *
+ * https://leetcode.cn/problems/rotate-image/description/
+ *
+ * algorithms
+ * Medium (74.66%)
+ * Likes:    1622
+ * Dislikes: 0
+ * Total Accepted:    447.8K
+ * Total Submissions: 599.5K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+ * 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+ *
+ * 示例 1：
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[[7,4,1],[8,5,2],[9,6,3]]
+ *
+ * 示例 2：
+ * 输入：matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+ * 输出：[[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+ *
+ * 提示：
+ * n == matrix.length == matrix[i].length
+ * 1 <= n <= 20
+ * -1000 <= matrix[i][j] <= 1000
+ */
+// @lc code=start
+class RotateImage {
+    /**
+     * 翻转主函数
+     */
+    public void rotate(int[][] matrix) {
+        // 题目已经做了 n == matrix.length == matrix[i].length 的限制，所以不需要判断
+        int n = matrix.length;
+
+        // 遍历到中间即可
+        for (int i = 0; i < n / 2; i++) {
+            // 遍历到 n - i - 1 即可
+            for (int j = i; j < n - i - 1; j++) {
+                // 上一个位置的横坐标
+                int lastI = i;
+                // 上一个位置的纵坐标
+                int lastJ = j;
+                // 上一个位置的数
+                int last = matrix[i][j];
+                // 翻转 4 次即可
+                for (int k = 0; k < 4; k++) {
+                    // 获取下一个翻转的位置
+                    int[] nextIndex = getNextIndex(n, lastI, lastJ);
+                    // 先把下一个位置的数取出来暂存
+                    int tmp = matrix[nextIndex[0]][nextIndex[1]];
+                    // 把上一个位置的数存入
+                    matrix[nextIndex[0]][nextIndex[1]] = last;
+                    // 把暂存的下一个位置的数赋值到上一个位置
+                    last = tmp;
+                    lastI = nextIndex[0];
+                    lastJ = nextIndex[1];
+                }
+            }
+        }
+    }
+
+    /**
+     * 获取下一个翻转的位置
+     * 通过数学找规律
+     */
+    private int[] getNextIndex(int n, int i, int j) {
+        return new int[] {j, n - i - 1};
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=54 lang=java
+ *
+ * [54] 螺旋矩阵
+ *
+ * https://leetcode.cn/problems/spiral-matrix/description/
+ *
+ * algorithms
+ * Medium (49.28%)
+ * Likes:    1376
+ * Dislikes: 0
+ * Total Accepted:    367.5K
+ * Total Submissions: 745.7K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * 给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。
+ *
+ * 示例 1：
+ * 输入：matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * 输出：[1,2,3,6,9,8,7,4,5]
+ *
+ * 示例 2：
+ * 输入：matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * 输出：[1,2,3,4,8,12,11,10,9,5,6,7]
+ *
+ * 提示：
+ * m == matrix.length
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
+ */
+// @lc code=start
+class SpiralMatrix {
+    /**
+     * 维护了四个变量，即矩阵的左边界、右边界、上边界和下边界。
+     * 在每个循环中，首先遍历上边界上的所有元素，然后遍历右边界上的所有元素。
+     * 如果左边界和右边界不相遇并且上边界和下边界不相遇，则遍历下边界和左边界上的所有元素。
+     * 然后将四个边界向内缩小，继续下一个循环，直到所有元素都被遍历完为止。
+     */
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) {
+            return res;
+        }
+        // 矩阵的行数
+        int m = matrix.length;
+        // 矩阵的列数
+        int n = matrix[0].length;
+        // 初始化边界
+        int left = 0, right = n - 1, top = 0, bottom = m - 1;
+        // 循环条件
+        while (left <= right && top <= bottom) {
+            // 遍历上边界
+            for (int j = left; j <= right; j ++) {
+                res.add(matrix[top][j]);
+            }
+            // 遍历右边界
+            for (int i = top + 1; i <= bottom; i ++) {
+                res.add(matrix[i][right]);
+            }
+            // 如果左右边界和上下边界没有相遇，遍历下边界和左边界
+            if (left < right && top < bottom) {
+                for (int j = right - 1; j >= left; j--) {
+                    res.add(matrix[bottom][j]);
+                }
+                for (int i = bottom - 1; i > top; i--) {
+                    res.add(matrix[i][left]);
+                }
+            }
+            // 缩小边界
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return res;
+    }
+}
+// @lc code=end
+
