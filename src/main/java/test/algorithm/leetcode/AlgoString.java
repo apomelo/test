b56adcfa73d4d42ef9f31beb9131ad578ce28a71
adream <listentonatural@gmail.com>
2023-05-08 09:33:26 +0800

    msg: algo string

--- src/main/java/test/algorithm/leetcode/AlgoString.java
+++ src/main/java/test/algorithm/leetcode/AlgoString.java
@@ -2,10 +2,7 @@ package test.algorithm.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * 字符串
@@ -34,6 +31,10 @@ public class AlgoString {
         // 字符串相乘
         log.info("MultiplyStrings: {}", new MultiplyStrings().multiply("2", "3"));
         log.info("MultiplyStrings: {}", new MultiplyStrings().multiply("123", "456"));
+        // 字母异位词分组
+        log.info("GroupAnagrams: {}", new GroupAnagrams().groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
+        log.info("GroupAnagrams: {}", new GroupAnagrams().groupAnagrams(new String[] {}));
+        log.info("GroupAnagrams: {}", new GroupAnagrams().groupAnagrams(new String[] {"a"}));
     }
 }
 
@@ -456,6 +457,72 @@ class MultiplyStrings {
         }
         String str = sb.toString();
         return str.length() == 0 ? "0" : str;
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=49 lang=java
+ *
+ * [49] 字母异位词分组
+ *
+ * https://leetcode.cn/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (67.82%)
+ * Likes:    1479
+ * Dislikes: 0
+ * Total Accepted:    457.7K
+ * Total Submissions: 675.3K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * 给你一个字符串数组，请你将 字母异位词 组合在一起。可以按任意顺序返回结果列表。
+ * 字母异位词 是由重新排列源单词的字母得到的一个新单词，所有源单词中的字母通常恰好只用一次。
+ *
+ * 示例 1:
+ * 输入: strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出: [["bat"],["nat","tan"],["ate","eat","tea"]]
+ *
+ * 示例 2:
+ * 输入: strs = [""]
+ * 输出: [[""]]
+ *
+ * 示例 3:
+ * 输入: strs = ["a"]
+ * 输出: [["a"]]
+ *
+ * 提示：
+ * 1 <= strs.length <= 10^4
+ * 0 <= strs[i].length <= 100
+ * strs[i] 仅包含小写字母
+ */
+// @lc code=start
+class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // 编码到分组的映射
+        Map<String, List<String>> codeToGroup = new HashMap<>();
+        for (String s : strs) {
+            // 对字符串进行编码
+            String code = encode(s);
+            // 把编码相同的字符串放在一起
+            codeToGroup.computeIfAbsent(code, k -> new LinkedList<>()).add(s);
+        }
+
+        // 返回结果
+        return new LinkedList<>(codeToGroup.values());
+    }
+
+    /**
+     * 利用每个字符的出现次数进行编码
+     */
+    public String encode(String s) {
+        char[] count = new char[26];
+        for (char c : s.toCharArray()) {
+            int delta = c - 'a';
+            count[delta]++;
+        }
+        return new String(count);
     }
 }
 // @lc code=end
