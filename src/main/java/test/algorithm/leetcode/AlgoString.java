ba2e426e845e5be16e1b12e731bb2a037506c622
adream <listentonatural@gmail.com>
2023-05-23 09:22:21 +0800

    msg: algo string

--- src/main/java/test/algorithm/leetcode/AlgoString.java
+++ src/main/java/test/algorithm/leetcode/AlgoString.java
@@ -35,6 +35,10 @@ public class AlgoString {
         log.info("GroupAnagrams: {}", new GroupAnagrams().groupAnagrams(new String[] {"eat", "tea", "tan", "ate", "nat", "bat"}));
         log.info("GroupAnagrams: {}", new GroupAnagrams().groupAnagrams(new String[] {}));
         log.info("GroupAnagrams: {}", new GroupAnagrams().groupAnagrams(new String[] {"a"}));
+        // 最后一个单词的长度
+        log.info("LengthOfLastWord: {}", new LengthOfLastWord().lengthOfLastWord("Hello World"));
+        log.info("LengthOfLastWord: {}", new LengthOfLastWord().lengthOfLastWord("   fly me   to   the moon  "));
+        log.info("LengthOfLastWord: {}", new LengthOfLastWord().lengthOfLastWord("luffy is still joyboy"));
     }
 }
 
@@ -526,3 +530,53 @@ class GroupAnagrams {
     }
 }
 // @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=58 lang=java
+ *
+ * [58] 最后一个单词的长度
+ *
+ * https://leetcode.cn/problems/length-of-last-word/description/
+ *
+ * algorithms
+ * Easy (42.55%)
+ * Likes:    588
+ * Dislikes: 0
+ * Total Accepted:    431.5K
+ * Total Submissions: 1M
+ * Testcase Example:  '"Hello World"'
+ *
+ * 给你一个字符串 s，由若干单词组成，单词前后用一些空格字符隔开。返回字符串中 最后一个 单词的长度。
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ *
+ * 示例 1：
+ * 输入：s = "Hello World"
+ * 输出：5
+ * 解释：最后一个单词是“World”，长度为5。
+ *
+ * 示例 2：
+ * 输入：s = "   fly me   to   the moon  "
+ * 输出：4
+ * 解释：最后一个单词是“moon”，长度为4。
+ *
+ * 示例 3：
+ * 输入：s = "luffy is still joyboy"
+ * 输出：6
+ * 解释：最后一个单词是长度为6的“joyboy”。
+ *
+ * 提示：
+ * 1 <= s.length <= 10^4
+ * s 仅有英文字母和空格 ' ' 组成
+ * s 中至少存在一个单词
+ */
+// @lc code=start
+class LengthOfLastWord {
+    public int lengthOfLastWord(String s) {
+        String[] split = s.trim().split("\\s+");
+        int n = split.length;
+        return split[n - 1].length();
+    }
+}
+// @lc code=end
+
