4412b0a0f0583ea093613a82210ba53d55ca0ab4
adream <listentonatural@gmail.com>
2023-05-29 20:18:48 +0800

    msg: algo dynamic programming

--- src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
+++ src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
@@ -2,6 +2,7 @@ package test.algorithm.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.Stack;
 
 /**
@@ -16,6 +17,18 @@ public class AlgoDynamicProgramming {
         log.info("LongestValidParentheses: {}", new LongestValidParentheses().longestValidParentheses("(()"));
         log.info("LongestValidParentheses: {}", new LongestValidParentheses().longestValidParentheses(")()())"));
         log.info("LongestValidParentheses: {}", new LongestValidParentheses().longestValidParentheses(""));
+        // 不同路径
+        log.info("UniquePaths: {}", new UniquePaths().uniquePaths(3, 7));
+        log.info("UniquePaths: {}", new UniquePaths().uniquePaths(3, 2));
+        log.info("UniquePaths: {}", new UniquePaths().uniquePaths(7, 3));
+        log.info("UniquePaths: {}", new UniquePaths().uniquePaths(3, 3));
+        // 不同路径 II
+        log.info("UniquePathsII: {}", new UniquePathsII().uniquePathsWithObstacles(new int[][]{{0,0,0},{0,1,0},{0,0,0}}));
+        log.info("UniquePathsII: {}", new UniquePathsII().uniquePathsWithObstacles(new int[][]{{0,1},{0,0}}));
+        log.info("UniquePathsII: {}", new UniquePathsII().uniquePathsWithObstacles(new int[][]{{0,0}}));
+        // 最小路径和
+        log.info("MinimumPathSum: {}", new MinimumPathSum().minPathSum(new int[][]{{1,3,1},{1,5,1},{4,2,1}}));
+        log.info("MinimumPathSum: {}", new MinimumPathSum().minPathSum(new int[][]{{1,2,3},{4,5,6}}));
     }
 }
 
@@ -89,6 +102,216 @@ class LongestValidParentheses {
             res = Math.max(res, j);
         }
         return res;
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=62 lang=java
+ *
+ * [62] 不同路径
+ *
+ * https://leetcode.cn/problems/unique-paths/description/
+ *
+ * algorithms
+ * Medium (67.72%)
+ * Likes:    1774
+ * Dislikes: 0
+ * Total Accepted:    623.1K
+ * Total Submissions: 919.9K
+ * Testcase Example:  '3\n7'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+ * 问总共有多少条不同的路径？
+ *
+ * 示例 1：
+ * 输入：m = 3, n = 7
+ * 输出：28
+ *
+ * 示例 2：
+ * 输入：m = 3, n = 2
+ * 输出：3
+ * 解释：
+ * 从左上角开始，总共有 3 条路径可以到达右下角。
+ * 1. 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右
+ * 3. 向下 -> 向右 -> 向下
+ *
+ * 示例 3：
+ * 输入：m = 7, n = 3
+ * 输出：28
+ *
+ * 示例 4：
+ * 输入：m = 3, n = 3
+ * 输出：6
+ *
+ * 提示：
+ * 1 <= m, n <= 100
+ * 题目数据保证答案小于等于 2 * 10^9
+ */
+// @lc code=start
+class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        if (m <= 1 || n <= 1) {
+            return 1;
+        }
+        int[][] dp = new int[m][n];
+        // 初始化状态
+        // 初始化第一列
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        // 初始化第一行
+        Arrays.fill(dp[0], 1);
+        // 状态转移
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=63 lang=java
+ *
+ * [63] 不同路径 II
+ *
+ * https://leetcode.cn/problems/unique-paths-ii/description/
+ *
+ * algorithms
+ * Medium (41.02%)
+ * Likes:    1056
+ * Dislikes: 0
+ * Total Accepted:    374.7K
+ * Total Submissions: 912.7K
+ * Testcase Example:  '[[0,0,0],[0,1,0],[0,0,0]]'
+ *
+ * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+ * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish”）。
+ * 现在考虑网格中有障碍物。那么从左上角到右下角将会有多少条不同的路径？
+ * 网格中的障碍物和空位置分别用 1 和 0 来表示。
+ *
+ * 示例 1：
+ * 输入：obstacleGrid = [[0,0,0],[0,1,0],[0,0,0]]
+ * 输出：2
+ * 解释：3x3 网格的正中间有一个障碍物。
+ * 从左上角到右下角一共有 2 条不同的路径：
+ * 1. 向右 -> 向右 -> 向下 -> 向下
+ * 2. 向下 -> 向下 -> 向右 -> 向右
+ *
+ * 示例 2：
+ * 输入：obstacleGrid = [[0,1],[0,0]]
+ * 输出：1
+ *
+ * 提示：
+ * m == obstacleGrid.length
+ * n == obstacleGrid[i].length
+ * 1 <= m, n <= 100
+ * obstacleGrid[i][j] 为 0 或 1
+ */
+// @lc code=start
+class UniquePathsII {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        // 排除格子为空的情况
+        if (obstacleGrid == null || obstacleGrid.length == 0 || obstacleGrid[0].length == 0) {
+            return 0;
+        }
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        // 初始化第一个位置
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+        // 初始化第一列
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] == 0 ? 0 : (obstacleGrid[i][0] == 1 ? 0 : 1);
+        }
+        // 初始化第一行
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] == 0 ? 0 : (obstacleGrid[0][j] == 1 ? 0 : 1);
+        }
+        // 状态转移
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=64 lang=java
+ *
+ * [64] 最小路径和
+ *
+ * https://leetcode.cn/problems/minimum-path-sum/description/
+ *
+ * algorithms
+ * Medium (69.49%)
+ * Likes:    1508
+ * Dislikes: 0
+ * Total Accepted:    486.3K
+ * Total Submissions: 699.7K
+ * Testcase Example:  '[[1,3,1],[1,5,1],[4,2,1]]'
+ *
+ * 给定一个包含非负整数的 m x n 网格 grid ，请找出一条从左上角到右下角的路径，使得路径上的数字总和为最小。
+ * 说明：每次只能向下或者向右移动一步。
+ *
+ * 示例 1：
+ * 输入：grid = [[1,3,1],[1,5,1],[4,2,1]]
+ * 输出：7
+ * 解释：因为路径 1→3→1→1→1 的总和最小。
+ *
+ * 示例 2：
+ * 输入：grid = [[1,2,3],[4,5,6]]
+ * 输出：12
+ *
+ * 提示：
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 200
+ * 0 <= grid[i][j] <= 200
+ */
+// @lc code=start
+class MinimumPathSum {
+    public int minPathSum(int[][] grid) {
+        // 排除格子为空的情况
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m + 1][n + 1];
+        // 初始化第一个位置
+        dp[0][0] = grid[0][0];
+        // 初始化第一列
+        for (int i = 1; i < m; i++) {
+            dp[i][0] = dp[i - 1][0] + grid[i][0];
+        }
+        // 初始化第一行
+        for (int j = 1; j < n; j++) {
+            dp[0][j] = dp[0][j - 1] + grid[0][j];
+        }
+        // 状态转移
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
     }
 }
 // @lc code=end
