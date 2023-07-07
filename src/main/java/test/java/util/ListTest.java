333ec6da6707c27c9378fc84635b0a71bcb0616a
adream <listentonatural@gmail.com>
2023-07-07 11:18:42 +0800

    msg: test list

--- src/main/java/test/java/util/ListTest.java
+++ src/main/java/test/java/util/ListTest.java
@@ -1,28 +1,28 @@
 package test.java.util;
 
+import com.google.common.collect.Lists;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
  * Created by C on 2019/11/7.
  */
+@Slf4j
 public class ListTest {
-    private static final Logger logger = LoggerFactory.getLogger(ListTest.class);
     public static void main(String[] args) {
         testStreamSorted();
         testForeach();
         testIterator();
+        testSublist();
+        testArraysAsList();
     }
 
     public static void testStreamSorted() {
+        log.info("---------- testStreamSorted begin ----------");
         List<A> list1 = new ArrayList<A>() {{
             add(new A(3, 5, 7));
             add(new A(3, 6, 8));
@@ -43,18 +43,20 @@ public class ListTest {
         sortList.addAll(list2);
 
         List<A> collect = sortList.stream().sorted(Comparator.comparing(A::getNum).reversed()).limit(10).collect(Collectors.toList());
-        logger.info("1: {}", collect);
+        log.info("1: {}", collect);
         List<A> collect2 = sortList.stream().sorted(Comparator.comparing(A::getNum).reversed().thenComparing(A::getId).thenComparing(A::getLevel).reversed()).limit(10).collect(Collectors.toList());
-        logger.info("2: {}", collect2);
+        log.info("2: {}", collect2);
         List<A> collect3 = sortList.stream().sorted(Comparator.comparing(A::getNum).reversed().thenComparing(A::getId).thenComparing(A::getLevel, Comparator.reverseOrder())).limit(10).collect(Collectors.toList());
-        logger.info("3: {}", collect3);
+        log.info("3: {}", collect3);
         List<A> collect4 = sortList.stream().sorted(Comparator.comparing(A::getNum, Comparator.reverseOrder()).thenComparing(A::getId).thenComparing(A::getLevel, Comparator.reverseOrder())).limit(10).collect(Collectors.toList());
-        logger.info("4: {}", collect4);
+        log.info("4: {}", collect4);
         List<A> collect5 = sortList.stream().sorted(Comparator.comparing(A::getNum).thenComparing(A::getLevel)).limit(10).collect(Collectors.toList());
-        logger.info("5: {}", collect5);
+        log.info("5: {}", collect5);
+        log.info("---------- testStreamSorted end ----------");
     }
 
     public static void testForeach() {
+        log.info("---------- testForeach begin ----------");
         List<A> aList = new ArrayList<>();
         aList.add(new A(1, 1, 1));
         aList.add(new A(2, 2, 2));
@@ -63,10 +65,12 @@ public class ListTest {
             a.id += 10;
         });
 
-        logger.info("{}", aList);
+        log.info("{}", aList);
+        log.info("---------- testForeach end ----------");
     }
 
     public static void testIterator() {
+        log.info("---------- testIterator begin ----------");
         List<Integer> list = new ArrayList<>();
         list.add(1);
         list.add(2);
@@ -75,12 +79,40 @@ public class ListTest {
         while (iterator.hasNext()) {
             Integer next = iterator.next();
             if (next == 2) {
-                logger.info("list.size={}", list.size());
+                log.info("list.size={}", list.size());
                 iterator.remove();
-                logger.info("list.size={}", list.size());
+                log.info("list.size={}", list.size());
             }
         }
+        log.info("---------- testIterator end ----------");
     }
+
+    private static void testSublist() {
+        log.info("---------- testSublist begin ----------");
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        log.info("list: {}", list);
+        list.subList((list.size() + 1) / 2, list.size()).clear();
+        log.info("list: {}", list);
+        log.info("---------- testSublist end ----------");
+    }
+
+    private static void testArraysAsList() {
+        log.info("---------- testArraysAsList begin ----------");
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+        try {
+            list.remove(0);
+        } catch (UnsupportedOperationException e) {
+            log.info("this arrays as list is not allow modify!!!, exception: ", e);
+        }
+        log.info("list: {}", list);
+        log.info("---------- testArraysAsList end ----------");
+    }
+
 
     @Data
     @AllArgsConstructor
