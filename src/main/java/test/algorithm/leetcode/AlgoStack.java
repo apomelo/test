23427eaf88043fb96c8dcda8d52c501dfb9aac86
adream <listentonatural@gmail.com>
2023-07-11 15:14:28 +0800

    msg: algo stack

--- src/main/java/test/algorithm/leetcode/AlgoStack.java
+++ src/main/java/test/algorithm/leetcode/AlgoStack.java
@@ -21,6 +21,11 @@ public class AlgoStack {
         // 柱状图中最大的矩形
         log.info("LargestRectangleInHistogram: {}", new LargestRectangleInHistogram().largestRectangleArea(new int[] {2,1,5,6,2,3}));
         log.info("LargestRectangleInHistogram: {}", new LargestRectangleInHistogram().largestRectangleArea(new int[] {2,4}));
+        // 柱状图中最大的矩形
+        log.info("MaximalRectangle: {}", new MaximalRectangle().maximalRectangle(new char[][] {{'1','0','1','0','0'}, {'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}}));
+        log.info("MaximalRectangle: {}", new MaximalRectangle().maximalRectangle(new char[][] {{}}));
+        log.info("MaximalRectangle: {}", new MaximalRectangle().maximalRectangle(new char[][] {{'0'}}));
+        log.info("MaximalRectangle: {}", new MaximalRectangle().maximalRectangle(new char[][] {{'1'}}));
     }
 }
 
@@ -123,6 +128,125 @@ class ValidParentheses {
 // @lc code=start
 class LargestRectangleInHistogram {
     public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n]; // 记录每个柱形条左边第一个小于它的柱形条的索引
+        int[] right = new int[n]; // 记录每个柱形条右边第一个小于它的柱形条的索引
+
+        Stack<Integer> stack = new Stack<>(); // 单调递增栈，存储柱形条的索引
+
+        // 遍历数组，找到每个柱形条左边第一个小于它的柱形条的索引
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear(); // 清空栈，用于下一次遍历
+
+        // 遍历数组，找到每个柱形条右边第一个小于它的柱形条的索引
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+
+        int maxArea = 0; // 记录最大面积
+
+        // 计算每个柱形条作为矩形的宽度，并计算对应的面积
+        for (int i = 0; i < n; i++) {
+            int width = right[i] - left[i] - 1; // 矩形的宽度为右边界索引减去左边界索引减一
+            int area = heights[i] * width; // 计算矩形的面积
+            maxArea = Math.max(maxArea, area); // 更新最大面积
+        }
+
+        return maxArea;
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=85 lang=java
+ *
+ * [85] 最大矩形
+ *
+ * https://leetcode.cn/problems/maximal-rectangle/description/
+ *
+ * algorithms
+ * Hard (54.69%)
+ * Likes:    1547
+ * Dislikes: 0
+ * Total Accepted:    178.5K
+ * Total Submissions: 326.4K
+ * Testcase Example:  '[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]'
+ *
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ *
+ * 示例 1：
+ * 输入：matrix =
+ * [["1","0","1","0","0"],
+ *  ["1","0","1","1","1"],
+ *  ["1","1","1","1","1"],
+ *  ["1","0","0","1","0"]]
+ * 输出：6
+ * 解释：最大矩形如上图所示。
+ *
+ * 示例 2：
+ * 输入：matrix = []
+ * 输出：0
+ *
+ * 示例 3：
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ *
+ * 示例 4：
+ * 输入：matrix = [["1"]]
+ * 输出：1
+ *
+ * 示例 5：
+ * 输入：matrix = [["0","0"]]
+ * 输出：0
+ *
+ * 提示：
+ * rows == matrix.length
+ * cols == matrix[0].length
+ * 1 <= row, cols <= 200
+ * matrix[i][j] 为 '0' 或 '1'
+ */
+// @lc code=start
+class MaximalRectangle {
+    public int maximalRectangle(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return 0;
+        }
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[] heights = new int[cols];
+        int maxArea = 0;
+
+        // 遍历每一行
+        for (int row = 0; row < rows; row++) {
+            // 更新 heights 数组
+            for (int col = 0; col < cols; col++) {
+                if (matrix[row][col] == '1') {
+                    heights[col]++;
+                } else {
+                    heights[col] = 0;
+                }
+            }
+            // 计算当前行 heights 数组对应的最大矩形面积
+            maxArea = Math.max(maxArea, largestRectangleArea(heights));
+        }
+
+        return maxArea;
+    }
+
+    private int largestRectangleArea(int[] heights) {
         int n = heights.length;
         int[] left = new int[n]; // 记录每个柱形条左边第一个小于它的柱形条的索引
         int[] right = new int[n]; // 记录每个柱形条右边第一个小于它的柱形条的索引
