1a2b703d94e02e6bf77a60cb79c59ad2a215041a
adream <listentonatural@gmail.com>
2023-07-17 09:43:05 +0800

    msg: algo two pointer

--- src/main/java/test/algorithm/leetcode/AlgoTwoPointer.java
+++ src/main/java/test/algorithm/leetcode/AlgoTwoPointer.java
@@ -51,6 +51,16 @@ public class AlgoTwoPointer {
         // 删除有序数组中的重复项 II
         log.info("RemoveDuplicatesFromSortedArrayII: {}", new RemoveDuplicatesFromSortedArrayII().removeDuplicates(new int[] {1,1,1,2,2,3}));
         log.info("RemoveDuplicatesFromSortedArrayII: {}", new RemoveDuplicatesFromSortedArrayII().removeDuplicates(new int[] {0,0,1,1,1,1,2,3,3}));
+        // 颜色分类
+        int[] mergeSortedArrayExample1 = new int[] {1,2,3,0,0,0};
+        new MergeSortedArray().merge(mergeSortedArrayExample1, 3, new int[] {2,5,6}, 3);
+        log.info("MergeSortedArray: {}", mergeSortedArrayExample1);
+        int[] mergeSortedArrayExample2 = new int[] {1};
+        new MergeSortedArray().merge(mergeSortedArrayExample2, 1, new int[] {}, 0);
+        log.info("MergeSortedArray: {}", mergeSortedArrayExample2);
+        int[] mergeSortedArrayExample3 = new int[] {0};
+        new MergeSortedArray().merge(mergeSortedArrayExample3, 0, new int[] {1}, 1);
+        log.info("MergeSortedArray: {}", mergeSortedArrayExample3);
     }
 }
 
@@ -900,6 +910,84 @@ class RemoveDuplicatesFromSortedArrayII {
         }
         // 数组长度为索引 + 1
         return slow + 1;
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=88 lang=java
+ *
+ * [88] 合并两个有序数组
+ *
+ * https://leetcode.cn/problems/merge-sorted-array/description/
+ *
+ * algorithms
+ * Easy (52.47%)
+ * Likes:    1927
+ * Dislikes: 0
+ * Total Accepted:    928.3K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '[1,2,3,0,0,0]\n3\n[2,5,6]\n3'
+ *
+ * 给你两个按 非递减顺序 排列的整数数组 nums1 和 nums2，另有两个整数 m 和 n ，分别表示 nums1 和 nums2 中的元素数目。
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m
+ * 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+ * 输出：[1,2,2,3,5,6]
+ * 解释：需要合并 [1,2,3] 和 [2,5,6] 。
+ * 合并结果是 [1,2,2,3,5,6] ，其中斜体加粗标注的为 nums1 中的元素。
+ *
+ * 示例 2：
+ * 输入：nums1 = [1], m = 1, nums2 = [], n = 0
+ * 输出：[1]
+ * 解释：需要合并 [1] 和 [] 。
+ * 合并结果是 [1] 。
+ *
+ * 示例 3：
+ * 输入：nums1 = [0], m = 0, nums2 = [1], n = 1
+ * 输出：[1]
+ * 解释：需要合并的数组是 [] 和 [1] 。
+ * 合并结果是 [1] 。
+ * 注意，因为 m = 0 ，所以 nums1 中没有元素。nums1 中仅存的 0 仅仅是为了确保合并结果可以顺利存放到 nums1 中。
+ *
+ * 提示：
+ * nums1.length == m + n
+ * nums2.length == n
+ * 0 <= m, n <= 200
+ * 1 <= m + n <= 200
+ * -10^9 <= nums1[i], nums2[j] <= 10^9
+ *
+ * 进阶：你可以设计实现一个时间复杂度为 O(m + n) 的算法解决此问题吗？
+ */
+// @lc code=start
+class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // 排除不合理情况
+        if (nums2.length != n || m + n != nums1.length) {
+            return;
+        }
+
+        // 两个指针分别初始化在两个数组的最后一个元素（类似拉链两端的锯齿）
+        int i = m - 1, j = n - 1;
+        // 生成排序的结果（类似拉链的拉锁）
+        int cur = nums1.length - 1;
+        // 从后向前生成结果数组，类似合并两个有序链表的逻辑
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[cur--] = nums1[i--];
+            } else {
+                nums1[cur--] = nums2[j--];
+            }
+        }
+        // 可能其中一个数组的指针走到尽头了，而另一个还没走完
+        // 因为我们本身就是在往 nums1 中放元素，所以只需考虑 nums2 是否剩元素即可
+        while (j >= 0) {
+            nums1[cur--] = nums2[j--];
+        }
     }
 }
 // @lc code=end
