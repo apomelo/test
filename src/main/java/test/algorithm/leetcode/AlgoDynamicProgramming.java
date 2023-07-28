3c9ef178a54e977b2a082d48366c1d0d62b019cb
adream <listentonatural@gmail.com>
2023-07-28 09:47:48 +0800

    msg: algo dynamic programming

--- src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
+++ src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
@@ -39,6 +39,10 @@ public class AlgoDynamicProgramming {
         log.info("DecodeWays: {}", new DecodeWays().numDecodings("12"));
         log.info("DecodeWays: {}", new DecodeWays().numDecodings("226"));
         log.info("DecodeWays: {}", new DecodeWays().numDecodings("06"));
+        // [97] 交错字符串
+        log.info("InterleavingString: {}", new InterleavingString().isInterleave("aabcc", "dbbca", "aadbbcbcac"));
+        log.info("InterleavingString: {}", new InterleavingString().isInterleave("aabcc", "dbbca", "aadbbbaccc"));
+        log.info("InterleavingString: {}", new InterleavingString().isInterleave("", "", ""));
     }
 }
 
@@ -542,6 +546,98 @@ class DecodeWays {
         }
 
         return dp[n];
+    }
+}
+// @lc code=end
+
+
+/**
+ * @lc app=leetcode.cn id=97 lang=java
+ *
+ * [97] 交错字符串
+ *
+ * https://leetcode.cn/problems/interleaving-string/description/
+ *
+ * algorithms
+ * Medium (44.65%)
+ * Likes:    888
+ * Dislikes: 0
+ * Total Accepted:    119.1K
+ * Total Submissions: 266.7K
+ * Testcase Example:  '"aabcc"\n"dbbca"\n"aadbbcbcac"'
+ *
+ * 给定三个字符串 s1、s2、s3，请你帮忙验证 s3 是否是由 s1 和 s2 交错 组成的。
+ * 两个字符串 s 和 t 交错 的定义与过程如下，其中每个字符串都会被分割成若干 非空 子字符串：
+ * s = s1 + s2 + ... + sn
+ * t = t1 + t2 + ... + tm
+ * |n - m| <= 1
+ * 交错 是 s1 + t1 + s2 + t2 + s3 + t3 + ... 或者 t1 + s1 + t2 + s2 + t3 + s3 +
+ * ...
+ *
+ * 注意：a + b 意味着字符串 a 和 b 连接。
+ *
+ * 示例 1：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbcbcac"
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：s1 = "aabcc", s2 = "dbbca", s3 = "aadbbbaccc"
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：s1 = "", s2 = "", s3 = ""
+ * 输出：true
+ *
+ * 提示：
+ * 0 <= s1.length, s2.length <= 100
+ * 0 <= s3.length <= 200
+ * s1、s2、和 s3 都由小写英文字母组成
+ *
+ * 进阶：您能否仅使用 O(s2.length) 额外的内存空间来解决它?
+ */
+// @lc code=start
+class InterleavingString {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+        // 如果长度对不上，必然不可能
+        if (m + n != s3.length()) {
+            return false;
+        }
+
+        // 备忘录，其中 -1 代表未计算，0 代表 false，1 代表 true
+        byte[][] memo = new byte[m + 1][n + 1];
+        for (byte[] row : memo) {
+            Arrays.fill(row, (byte) -1);
+        }
+        return dp(s1, 0, s2, 0, s3, memo);
+    }
+
+    // 定义：计算 s1[i..] 和 s2[j..] 是否能组合出 s3[i+j..]
+    private boolean dp(String s1, int i, String s2, int j, String s3, byte[][] memo) {
+        int k = i + j;
+        // base case，s3 构造完成
+        if (k == s3.length()) {
+            return true;
+        }
+        // 查备忘录，如果已经计算过，直接返回
+        if (memo[i][j] != -1) {
+            return memo[i][j] == 1;
+        }
+
+        boolean res = false;
+        // 如果，s1[i] 可以匹配 s3[k]，那么填入 s1[i] 试一下
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k)) {
+            res = dp(s1, i + 1, s2, j, s3, memo);
+        }
+        // 如果，s2[j] 可以匹配，那么填入 s2[j] 试一下
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k)) {
+            res |= dp(s1, i, s2, j + 1, s3, memo);
+        }
+        // 如果 s1[i] 和 s2[j] 都匹配不了，则返回 false
+        // 将结果存入备忘录
+        memo[i][j] = (byte) (res ? 1 : 0);
+        return res;
     }
 }
 // @lc code=end
