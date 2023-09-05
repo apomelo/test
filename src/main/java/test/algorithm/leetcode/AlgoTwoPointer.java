c4d564ee22d7053b6ec100507ca8f99dc84887a8
adream <listentonatural@gmail.com>
2023-09-05 09:34:37 +0800

    msg: algo two pointer

--- src/main/java/test/algorithm/leetcode/AlgoTwoPointer.java
+++ src/main/java/test/algorithm/leetcode/AlgoTwoPointer.java
@@ -61,6 +61,10 @@ public class AlgoTwoPointer {
         int[] mergeSortedArrayExample3 = new int[] {0};
         new MergeSortedArray().merge(mergeSortedArrayExample3, 0, new int[] {1}, 1);
         log.info("MergeSortedArray: {}", mergeSortedArrayExample3);
+        // [125] 验证回文串
+        log.info("ValidPalindrome: {}", new ValidPalindrome().isPalindrome("A man, a plan, a canal: Panama"));
+        log.info("ValidPalindrome: {}", new ValidPalindrome().isPalindrome("race a car"));
+        log.info("ValidPalindrome: {}", new ValidPalindrome().isPalindrome(" "));
     }
 }
 
@@ -968,5 +972,66 @@ class MergeSortedArray {
         while (j >= 0) {
             nums1[cur--] = nums2[j--];
         }
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=125 lang=java
+ *
+ * [125] 验证回文串
+ *
+ * https://leetcode.cn/problems/valid-palindrome/description/
+ *
+ * algorithms
+ * Easy (46.47%)
+ * Likes:    683
+ * Dislikes: 0
+ * Total Accepted:    498K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '"A man, a plan, a canal: Panama"'
+ *
+ * 如果在将所有大写字符转换为小写字符、并移除所有非字母数字字符之后，短语正着读和反着读都一样。则可以认为该短语是一个 回文串 。
+ * 字母和数字都属于字母数字字符。
+ * 给你一个字符串 s，如果它是 回文串 ，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入: s = "A man, a plan, a canal: Panama"
+ * 输出：true
+ * 解释："amanaplanacanalpanama" 是回文串。
+ *
+ * 示例 2：
+ * 输入：s = "race a car"
+ * 输出：false
+ * 解释："raceacar" 不是回文串。
+ *
+ * 示例 3：
+ * 输入：s = " "
+ * 输出：true
+ * 解释：在移除非字母数字字符之后，s 是一个空字符串 "" 。
+ * 由于空字符串正着反着读都一样，所以是回文串。
+ *
+ * 提示：
+ * 1 <= s.length <= 2 * 10^5
+ * s 仅由可打印的 ASCII 字符组成
+ */
+class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        // 把非字母去掉
+        String regex = "[^a-zA-Z0-9]";
+        String replaced = s.replaceAll(regex, "");
+        // 转换成小写
+        String target = replaced.toLowerCase();
+        if (target.isEmpty()) {
+            return true;
+        }
+        // 双指针判断
+        int left = 0, right = target.length() - 1;
+        while (left < right) {
+            if (target.charAt(left++) != target.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
