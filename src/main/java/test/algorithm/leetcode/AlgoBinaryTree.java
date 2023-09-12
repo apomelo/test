6facf9ed2a01a163ef6e4eed7503c6a57a4382ee
adream <listentonatural@gmail.com>
2023-09-12 09:24:06 +0800

    msg: algo binary tree

--- src/main/java/test/algorithm/leetcode/AlgoBinaryTree.java
+++ src/main/java/test/algorithm/leetcode/AlgoBinaryTree.java
@@ -114,6 +114,9 @@ public class AlgoBinaryTree {
         log.info("BinaryTreeMaximumPathSum: {}", new BinaryTreeMaximumPathSum().maxPathSum(BinaryTreeMaximumPathSum.example1()));
         log.info("BinaryTreeMaximumPathSum: {}", new BinaryTreeMaximumPathSum().maxPathSum(BinaryTreeMaximumPathSum.example2()));
         log.info("BinaryTreeMaximumPathSum: {}", new BinaryTreeMaximumPathSum().maxPathSum(BinaryTreeMaximumPathSum.example3()));
+        // [129] 求根节点到叶节点数字之和
+        log.info("SumRootToLeafNumbers: {}", new SumRootToLeafNumbers().sumNumbers(SumRootToLeafNumbers.example1()));
+        log.info("SumRootToLeafNumbers: {}", new SumRootToLeafNumbers().sumNumbers(SumRootToLeafNumbers.example2()));
     }
 }
 
@@ -1912,5 +1915,86 @@ class BinaryTreeMaximumPathSum {
     }
     public static TreeNode example3() {
         return new TreeNode(1, new TreeNode(-2, new TreeNode(1, new TreeNode(-1), null), new TreeNode(3)), new TreeNode(-3, new TreeNode(-2), null));
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=129 lang=java
+ *
+ * [129] 求根节点到叶节点数字之和
+ *
+ * https://leetcode.cn/problems/sum-root-to-leaf-numbers/description/
+ *
+ * algorithms
+ * Medium (70.09%)
+ * Likes:    680
+ * Dislikes: 0
+ * Total Accepted:    225.5K
+ * Total Submissions: 321.6K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给你一个二叉树的根节点 root ，树中每个节点都存放有一个 0 到 9 之间的数字。
+ * 每条从根节点到叶节点的路径都代表一个数字：
+ * 例如，从根节点到叶节点的路径 1 -> 2 -> 3 表示数字 123 。
+ * 计算从根节点到叶节点生成的 所有数字之和 。
+ * 叶节点 是指没有子节点的节点。
+ *
+ * 示例 1：
+ * 输入：root = [1,2,3]
+ * 输出：25
+ * 解释：
+ * 从根到叶子节点路径 1->2 代表数字 12
+ * 从根到叶子节点路径 1->3 代表数字 13
+ * 因此，数字总和 = 12 + 13 = 25
+ *
+ * 示例 2：
+ * 输入：root = [4,9,0,5,1]
+ * 输出：1026
+ * 解释：
+ * 从根到叶子节点路径 4->9->5 代表数字 495
+ * 从根到叶子节点路径 4->9->1 代表数字 491
+ * 从根到叶子节点路径 4->0 代表数字 40
+ * 因此，数字总和 = 495 + 491 + 40 = 1026
+ *
+ * 提示：
+ * 树中节点的数目在范围 [1, 1000] 内
+ * 0 <= Node.val <= 9
+ * 树的深度不超过 10
+ */
+class SumRootToLeafNumbers {
+    private int sum = 0;
+
+    public int sumNumbers(TreeNode root) {
+        StringBuilder track = new StringBuilder();
+        backtrack(root, track);
+        return sum;
+    }
+
+    private void backtrack(TreeNode root, StringBuilder track) {
+        if (root == null) {
+            return;
+        }
+
+        // 选择
+        track.append(root.val);
+        if (root.left == null && root.right == null) {
+            sum += Integer.parseInt(track.toString());
+        }
+
+        // 进入下一层决策树
+        backtrack(root.left, track);
+        backtrack(root.right, track);
+
+        // 撤销选择
+        track.deleteCharAt(track.length() - 1);
+    }
+
+
+    public static TreeNode example1() {
+        return new TreeNode(1, new TreeNode(2), new TreeNode(3));
+    }
+    public static TreeNode example2() {
+        return new TreeNode(4, new TreeNode(9, new TreeNode(5), new TreeNode(1)), new TreeNode(0));
     }
 }
