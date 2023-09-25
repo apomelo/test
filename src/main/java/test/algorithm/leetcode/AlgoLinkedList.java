40c06c5d4a10b0ef988b7c4ed47a2d588b42462d
adream <listentonatural@gmail.com>
2023-09-25 10:30:22 +0800

    msg: algo linked list

--- src/main/java/test/algorithm/leetcode/AlgoLinkedList.java
+++ src/main/java/test/algorithm/leetcode/AlgoLinkedList.java
@@ -2,6 +2,8 @@ package test.algorithm.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 
+import test.algorithm.leetcode.AlgoLinkedList.Node;
+
 /**
  * 链表
  * @author C
@@ -41,6 +43,29 @@ public class AlgoLinkedList {
         log.info("ReverseLinkedListII: {}", new ReverseLinkedListII().reverseBetween(ReverseLinkedListII.example1(), 2, 4));
         log.info("ReverseLinkedListII: {}", new ReverseLinkedListII().reverseBetween(ReverseLinkedListII.example2(), 1, 1));
         log.info("ReverseLinkedListII: {}", new ReverseLinkedListII().reverseBetween(ReverseLinkedListII.example3(), 1, 2));
+        // [138] 复制带随机指针的链表
+        log.info("CopyListWithRandomPointer: {}", new CopyListWithRandomPointer().copyRandomList(CopyListWithRandomPointer.example1()));
+        log.info("CopyListWithRandomPointer: {}", new CopyListWithRandomPointer().copyRandomList(CopyListWithRandomPointer.example2()));
+        log.info("CopyListWithRandomPointer: {}", new CopyListWithRandomPointer().copyRandomList(CopyListWithRandomPointer.example3()));
+    }
+
+    static class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+
+        @Override
+        public String toString() {
+            return random != null
+                    ? "[" + val + ", " + random.val + "], " + next
+                    : "[" + val + ", null], " + next;
+        }
     }
 }
 
@@ -675,5 +700,129 @@ class ReverseLinkedListII {
 
     public static ListNode example3() {
         return new ListNode(3, new ListNode(5));
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=138 lang=java
+ *
+ * [138] 复制带随机指针的链表
+ *
+ * https://leetcode.cn/problems/copy-list-with-random-pointer/description/
+ *
+ * algorithms
+ * Medium (66.02%)
+ * Likes:    1220
+ * Dislikes: 0
+ * Total Accepted:    217.8K
+ * Total Submissions: 329.9K
+ * Testcase Example:  '[[7,null],[13,0],[11,4],[10,2],[1,0]]'
+ *
+ * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+ * 构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random
+ * 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+ * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+ * 返回复制链表的头节点。
+ *
+ * 用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+ * val：一个表示 Node.val 的整数。
+ * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+ * 你的代码 只 接受原链表的头节点 head 作为传入参数。
+ *
+ * 示例 1：
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ *
+ * 示例 2：
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ *
+ * 示例 3：
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ *
+ * 提示：
+ * 0 <= n <= 1000
+ * -10^4 <= Node.val <= 10^4
+ * Node.random 为 null 或指向链表中的节点。
+ *
+ * 注意：本题与主站 138 题相同：https://leetcode-cn.com/problems/copy-list-with-random-pointer/
+ */
+class CopyListWithRandomPointer {
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        // 复制链表
+        Node p = head;
+        while (p != null) {
+            Node copy = new Node(p.val);
+            Node next = p.next;
+            p.next = copy;
+            copy.next = next;
+            p = next;
+        }
+        // 复制随机节点
+        p = head;
+        while (p != null) {
+            // 复制随机节点
+            p.next.random = p.random == null ? null : p.random.next;
+            // 指针前进
+            p = p.next.next;
+        }
+        // 拆分链表
+        p = head;
+        Node copyHead = new Node(-1);
+        Node copyP = copyHead;
+        while (p != null) {
+            // 连接复制的链表节点
+            copyP.next = p.next;
+            // 删除复制的链表节点
+            p.next = p.next.next;
+            // 复制链表指针前进
+            copyP = copyP.next;
+            copyP.next = null;
+            // 原链表指针前进
+            p = p.next;
+        }
+        return copyHead.next;
+    }
+
+
+    public static Node example1() {
+        Node node0 = new Node(7);
+        Node node1 = new Node(13);
+        Node node2 = new Node(11);
+        Node node3 = new Node(10);
+        Node node4 = new Node(1);
+        node0.next = node1;
+        node1.next = node2;
+        node1.random = node0;
+        node2.next = node3;
+        node2.random = node4;
+        node3.next = node4;
+        node3.random = node2;
+        node4.random = node0;
+        return node0;
+    }
+
+    public static Node example2() {
+        Node node0 = new Node(1);
+        Node node1 = new Node(2);
+        node0.next = node1;
+        node0.random = node1;
+        node1.random = node1;
+        return node0;
+    }
+
+    public static Node example3() {
+        Node node0 = new Node(3);
+        Node node1 = new Node(3);
+        Node node2 = new Node(3);
+        node0.next = node1;
+        node1.next = node2;
+        node1.random = node0;
+        return node0;
     }
 }
