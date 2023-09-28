bd76d2a702f68106fc5aaa1b437821fb18e88ec1
adream <listentonatural@gmail.com>
2023-09-28 12:03:06 +0800

    msg: algo linked list

--- src/main/java/test/algorithm/leetcode/AlgoLinkedList.java
+++ src/main/java/test/algorithm/leetcode/AlgoLinkedList.java
@@ -47,6 +47,14 @@ public class AlgoLinkedList {
         log.info("CopyListWithRandomPointer: {}", new CopyListWithRandomPointer().copyRandomList(CopyListWithRandomPointer.example1()));
         log.info("CopyListWithRandomPointer: {}", new CopyListWithRandomPointer().copyRandomList(CopyListWithRandomPointer.example2()));
         log.info("CopyListWithRandomPointer: {}", new CopyListWithRandomPointer().copyRandomList(CopyListWithRandomPointer.example3()));
+        // [141] 环形链表
+        log.info("LinkedListCycle: {}", new LinkedListCycle().hasCycle(LinkedListCycle.example1()));
+        log.info("LinkedListCycle: {}", new LinkedListCycle().hasCycle(LinkedListCycle.example2()));
+        log.info("LinkedListCycle: {}", new LinkedListCycle().hasCycle(LinkedListCycle.example3()));
+        // [142] 环形链表 II
+        log.info("LinkedListCycleII: {}", new LinkedListCycleII().detectCycle(LinkedListCycle.example1()));
+        log.info("LinkedListCycleII: {}", new LinkedListCycleII().detectCycle(LinkedListCycle.example2()));
+        log.info("LinkedListCycleII: {}", new LinkedListCycleII().detectCycle(LinkedListCycle.example3()));
     }
 
     static class Node {
@@ -824,5 +832,181 @@ class CopyListWithRandomPointer {
         node1.next = node2;
         node1.random = node0;
         return node0;
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=141 lang=java
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode.cn/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (51.85%)
+ * Likes:    2007
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 2.1M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给你一个链表的头节点 head ，判断链表中是否有环。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos
+ * 来表示链表尾连接到链表中的位置（索引从 0 开始）。注意：pos 不作为参数进行传递 。仅仅是为了标识链表的实际情况。
+ * 如果链表中存在环 ，则返回 true 。 否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ * 示例 2：
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ * 示例 3：
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ *
+ * 提示：
+ * 链表中节点的数目范围是 [0, 10^4]
+ * -10^5 <= Node.val <= 10^5
+ * pos 为 -1 或者链表中的一个 有效索引 。
+ *
+ * 进阶：你能用 O(1)（即，常量）内存解决此问题吗？
+ */
+class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static ListNode example1() {
+        ListNode listNode = new ListNode(3);
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(0);
+        ListNode listNode3 = new ListNode(-4);
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode1;
+        return listNode;
+    }
+
+    public static ListNode example2() {
+        ListNode listNode = new ListNode(1);
+        ListNode listNode1 = new ListNode(2);
+        listNode.next = listNode1;
+        listNode1.next = listNode;
+        return listNode;
+    }
+
+    public static ListNode example3() {
+        return new ListNode(1);
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ *
+ * https://leetcode.cn/problems/linked-list-cycle-ii/description/
+ *
+ * algorithms
+ * Medium (57.67%)
+ * Likes:    2337
+ * Dislikes: 0
+ * Total Accepted:    751K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos
+ * 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos
+ * 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+ * 不允许修改 链表。
+ *
+ * 示例 1：
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：返回索引为 1 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ *
+ * 示例 2：
+ * 输入：head = [1,2], pos = 0
+ * 输出：返回索引为 0 的链表节点
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ *
+ * 示例 3：
+ * 输入：head = [1], pos = -1
+ * 输出：返回 null
+ * 解释：链表中没有环。
+ *
+ * 提示：
+ * 链表中节点的数目范围在范围 [0, 10^4] 内
+ * -10^5 <= Node.val <= 10^5
+ * pos 的值为 -1 或者链表中的一个有效索引
+ *
+ * 进阶：你是否可以使用 O(1) 空间解决此题？
+ */
+class LinkedListCycleII {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) break;
+        }
+        // 上面的代码类似 hasCycle 函数
+        if (fast == null || fast.next == null) {
+            // fast 遇到空指针说明没有环
+            return null;
+        }
+
+        // 重新指向头结点
+        slow = head;
+        // 快慢指针同步前进，相交点就是环起点
+        while (slow != fast) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    public static ListNode example1() {
+        ListNode listNode = new ListNode(3);
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(0);
+        ListNode listNode3 = new ListNode(-4);
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode1;
+        return listNode;
+    }
+
+    public static ListNode example2() {
+        ListNode listNode = new ListNode(1);
+        ListNode listNode1 = new ListNode(2);
+        listNode.next = listNode1;
+        listNode1.next = listNode;
+        return listNode;
+    }
+
+    public static ListNode example3() {
+        return new ListNode(1);
     }
 }
