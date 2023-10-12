1bc0ac7f80e4ec7305fb6af245b7dec8d03a1fae
adream <listentonatural@gmail.com>
2023-10-12 10:10:17 +0800

    msg: algo dynamic programming

--- src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
+++ src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
@@ -72,6 +72,9 @@ public class AlgoDynamicProgramming {
         log.info("WordBreakII: {}", new WordBreakII().wordBreak("catsanddog", Arrays.asList("cat","cats","and","sand","dog")));
         log.info("WordBreakII: {}", new WordBreakII().wordBreak("pineapplepenapple", Arrays.asList("apple","pen","applepen","pine","pineapple")));
         log.info("WordBreakII: {}", new WordBreakII().wordBreak("catsandog", Arrays.asList("cats","dog","sand","and","cat")));
+        // [152] 乘积最大子数组
+        log.info("MaximumProductSubarray: {}", new MaximumProductSubarray().maxProduct(new int[] {2,3,-2,4}));
+        log.info("MaximumProductSubarray: {}", new MaximumProductSubarray().maxProduct(new int[] {-2,0,-1}));
     }
 }
 
@@ -1404,5 +1407,66 @@ class WordBreakII {
             dp[start] = -1;
         }
         return dp[start] == 1;
+    }
+}
+
+
+/*
+ * @lc app=leetcode.cn id=152 lang=java
+ *
+ * [152] 乘积最大子数组
+ *
+ * https://leetcode.cn/problems/maximum-product-subarray/description/
+ *
+ * algorithms
+ * Medium (43.12%)
+ * Likes:    2121
+ * Dislikes: 0
+ * Total Accepted:    384.1K
+ * Total Submissions: 890.8K
+ * Testcase Example:  '[2,3,-2,4]'
+ *
+ * 给你一个整数数组 nums ，请你找出数组中乘积最大的非空连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+ * 测试用例的答案是一个 32-位 整数。
+ * 子数组 是数组的连续子序列。
+ *
+ * 示例 1:
+ * 输入: nums = [2,3,-2,4]
+ * 输出: 6
+ * 解释: 子数组 [2,3] 有最大乘积 6。
+ *
+ * 示例 2:
+ * 输入: nums = [-2,0,-1]
+ * 输出: 0
+ * 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+ *
+ * 提示:
+ * 1 <= nums.length <= 2 * 10^4
+ * -10 <= nums[i] <= 10
+ * nums 的任何前缀或后缀的乘积都 保证 是一个 32-位 整数
+ */
+class MaximumProductSubarray {
+    public int maxProduct(int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+
+        int length = nums.length;
+        // dp[i][0] 表示以 i 为终点的子数组最大乘积
+        // dp[i][1] 表示以 i 为终点的子数组最小乘积
+        int[][] dp = new int[length][2];
+        // base case
+        dp[0][0] = nums[0];
+        dp[0][1] = nums[0];
+        // 最大子数组乘积
+        int res = nums[0];
+        for (int i = 1; i < length; i++) {
+            int a = nums[i] * dp[i - 1][0];
+            int b = nums[i] * dp[i - 1][1];
+            dp[i][0] = Math.max(nums[i], Math.max(a, b));
+            dp[i][1] = Math.min(nums[i], Math.min(a, b));
+            res = Math.max(res, dp[i][0]);
+        }
+        return res;
     }
 }
