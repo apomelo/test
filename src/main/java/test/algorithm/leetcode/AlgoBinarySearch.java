504071349056354687d82667327c3c0aaaf1642a
adream <listentonatural@gmail.com>
2023-10-13 09:49:10 +0800

    msg: algo binary search

--- src/main/java/test/algorithm/leetcode/AlgoBinarySearch.java
+++ src/main/java/test/algorithm/leetcode/AlgoBinarySearch.java
@@ -10,27 +10,36 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AlgoBinarySearch {
     public static void main(String[] args) {
-        // 搜索旋转排序数组
+        // [33] 搜索旋转排序数组
         log.info("SearchInRotatedSortedArray: {}", new SearchInRotatedSortedArray().search(new int[] {4,5,6,7,0,1,2}, 0));
         log.info("SearchInRotatedSortedArray: {}", new SearchInRotatedSortedArray().search(new int[] {4,5,6,7,0,1,2}, 3));
         log.info("SearchInRotatedSortedArray: {}", new SearchInRotatedSortedArray().search(new int[] {5,1,3}, 5));
         log.info("SearchInRotatedSortedArray: {}", new SearchInRotatedSortedArray().search(new int[] {4,5,6,7,8,1,2,3}, 8));
-        // 在排序数组中查找元素的第一个和最后一个位置
+        // [34] 在排序数组中查找元素的第一个和最后一个位置
         log.info("FindFirstAndLastPositionOfElementInSortedArray: {}", new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[] {5,7,7,8,8,10}, 8));
         log.info("FindFirstAndLastPositionOfElementInSortedArray: {}", new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[] {5,7,7,8,8,10}, 6));
         log.info("FindFirstAndLastPositionOfElementInSortedArray: {}", new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[] {}, 0));
         log.info("FindFirstAndLastPositionOfElementInSortedArray: {}", new FindFirstAndLastPositionOfElementInSortedArray().searchRange(new int[] {1}, 1));
-        // 搜索插入位置
+        // [35] 搜索插入位置
         log.info("SearchInsertPosition: {}", new SearchInsertPosition().searchInsert(new int[] {1,3,5,6}, 5));
         log.info("SearchInsertPosition: {}", new SearchInsertPosition().searchInsert(new int[] {1,3,5,6}, 2));
         log.info("SearchInsertPosition: {}", new SearchInsertPosition().searchInsert(new int[] {1,3,5,6}, 7));
         log.info("SearchInsertPosition: {}", new SearchInsertPosition().searchInsert(new int[] {1,3,5,6}, 0));
-        // 搜索二维矩阵
+        // [74] 搜索二维矩阵
         log.info("SearchA2dMatrix: {}", new SearchA2dMatrix().searchMatrix(new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 3));
         log.info("SearchA2dMatrix: {}", new SearchA2dMatrix().searchMatrix(new int[][] {{1,3,5,7},{10,11,16,20},{23,30,34,60}}, 13));
-        // 搜索旋转排序数组 II
+        // [81] 搜索旋转排序数组 II
         log.info("SearchInRotatedSortedArrayII: {}", new SearchInRotatedSortedArrayII().search(new int[] {2,5,6,0,0,1,2}, 0));
         log.info("SearchInRotatedSortedArrayII: {}", new SearchInRotatedSortedArrayII().search(new int[] {2,5,6,0,0,1,2}, 3));
+        // [153] 寻找旋转排序数组中的最小值
+        log.info("FindMinimumInRotatedSortedArray: {}", new FindMinimumInRotatedSortedArray().findMin(new int[] {3,4,5,1,2}));
+        log.info("FindMinimumInRotatedSortedArray: {}", new FindMinimumInRotatedSortedArray().findMin(new int[] {4,5,6,7,0,1,2}));
+        log.info("FindMinimumInRotatedSortedArray: {}", new FindMinimumInRotatedSortedArray().findMin(new int[] {11,13,15,17}));
+        log.info("FindMinimumInRotatedSortedArray: {}", new FindMinimumInRotatedSortedArray().findMin(new int[] {3,1,2}));
+        // [154] 寻找旋转排序数组中的最小值 II
+        log.info("FindMinimumInRotatedSortedArrayII: {}", new FindMinimumInRotatedSortedArrayII().findMin(new int[] {1,3,5}));
+        log.info("FindMinimumInRotatedSortedArrayII: {}", new FindMinimumInRotatedSortedArrayII().findMin(new int[] {2,2,2,0,1}));
+        log.info("FindMinimumInRotatedSortedArrayII: {}", new FindMinimumInRotatedSortedArrayII().findMin(new int[] {1,2,1}));
     }
 }
 
@@ -398,5 +407,152 @@ class SearchInRotatedSortedArrayII {
             }
         }
         return false;
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=153 lang=java
+ *
+ * [153] 寻找旋转排序数组中的最小值
+ *
+ * https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (57.14%)
+ * Likes:    1033
+ * Dislikes: 0
+ * Total Accepted:    400.6K
+ * Total Submissions: 701.1K
+ * Testcase Example:  '[3,4,5,1,2]'
+ *
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7]
+ * 在变化后可能得到：
+ * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+ * 若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+ *
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * 给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+ * 你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+ *
+ * 示例 1：
+ * 输入：nums = [3,4,5,1,2]
+ * 输出：1
+ * 解释：原数组为 [1,2,3,4,5] ，旋转 3 次得到输入数组。
+ *
+ * 示例 2：
+ * 输入：nums = [4,5,6,7,0,1,2]
+ * 输出：0
+ * 解释：原数组为 [0,1,2,4,5,6,7] ，旋转 4 次得到输入数组。
+ *
+ * 示例 3：
+ * 输入：nums = [11,13,15,17]
+ * 输出：11
+ * 解释：原数组为 [11,13,15,17] ，旋转 4 次得到输入数组。
+ *
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 5000
+ * -5000 <= nums[i] <= 5000
+ * nums 中的所有整数 互不相同
+ * nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ */
+class FindMinimumInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        // 判断不合法情况
+        if (nums == null) {
+            return 0;
+        }
+        // 定义左右指针
+        int length = nums.length;
+        int left = 0, right = length - 1;
+        while (left < right) {
+            // 因为 nums 中元素不重复，所以当 nums[left] < nums[right] 时表示该子段不包含翻转段
+            if (nums[left] < nums[right]) {
+                break;
+            }
+            // 找出中点（奇数是是中点，偶数时是中间两个数左边那个）
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[left]) {
+                right = mid;
+            } else {
+                // 因为上面找中点的方式，剩两个数时会陷入死循环，这里需要 +1
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+}
+
+
+/**
+ * @lc app=leetcode.cn id=154 lang=java
+ *
+ * [154] 寻找旋转排序数组中的最小值 II
+ *
+ * https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array-ii/description/
+ *
+ * algorithms
+ * Hard (52.41%)
+ * Likes:    642
+ * Dislikes: 0
+ * Total Accepted:    180.5K
+ * Total Submissions: 344.4K
+ * Testcase Example:  '[1,3,5]'
+ *
+ * 已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,4,4,5,6,7]
+ * 在变化后可能得到：
+ * 若旋转 4 次，则可以得到 [4,5,6,7,0,1,4]
+ * 若旋转 7 次，则可以得到 [0,1,4,4,5,6,7]
+ *
+ * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+ * 给你一个可能存在 重复 元素值的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+ * 你必须尽可能减少整个过程的操作步骤。
+ *
+ * 示例 1：
+ * 输入：nums = [1,3,5]
+ * 输出：1
+ *
+ * 示例 2：
+ * 输入：nums = [2,2,2,0,1]
+ * 输出：0
+ *
+ * 提示：
+ * n == nums.length
+ * 1 <= n <= 5000
+ * -5000 <= nums[i] <= 5000
+ * nums 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+ *
+ * 进阶：这道题与 寻找旋转排序数组中的最小值 类似，但 nums 可能包含重复元素。允许重复会影响算法的时间复杂度吗？会如何影响，为什么？
+ */
+class FindMinimumInRotatedSortedArrayII {
+    public int findMin(int[] nums) {
+        // 判断不合法情况
+        if (nums == null) {
+            return 0;
+        }
+        // 定义左右指针
+        int length = nums.length;
+        int left = 0, right = length - 1;
+        while (left < right) {
+            // 当 nums[left] < nums[right] 时表示左边是正序
+            if (nums[left] < nums[right]) {
+                break;
+            }
+            // 因为 nums 中元素可重复，所以当 nums[left] == nums[right] 时需要移动一边指针
+            if (nums[left] == nums[right]) {
+                left++;
+                continue;
+            }
+            // 找出中点（奇数是是中点，偶数时是中间两个数左边那个）
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[left]) {
+                right = mid;
+            } else {
+                // 因为上面找中点的方式，剩两个数时会陷入死循环，这里需要 +1
+                left = mid + 1;
+            }
+        }
+        return nums[left];
     }
 }
