a08830f706a6a27160d0fc6eb7307341c6e2a1cf
adream <listentonatural@gmail.com>
2023-10-23 09:33:08 +0800

    msg: algo string

--- src/main/java/test/algorithm/leetcode/AlgoString.java
+++ src/main/java/test/algorithm/leetcode/AlgoString.java
@@ -61,6 +61,10 @@ public class AlgoString {
         log.info("ReverseWordsInAString: {}", new ReverseWordsInAString().reverseWords("the sky is blue"));
         log.info("ReverseWordsInAString: {}", new ReverseWordsInAString().reverseWords("  hello world  "));
         log.info("ReverseWordsInAString: {}", new ReverseWordsInAString().reverseWords("a good   example"));
+        // [165] 比较版本号
+        log.info("CompareVersionNumbers: {}", new CompareVersionNumbers().compareVersion("1.01", "1.001"));
+        log.info("CompareVersionNumbers: {}", new CompareVersionNumbers().compareVersion("1.0", "1.0.0"));
+        log.info("CompareVersionNumbers: {}", new CompareVersionNumbers().compareVersion("0.1", "1.1"));
     }
 }
 
@@ -1111,5 +1115,81 @@ class ReverseWordsInAString {
             sb.append(split[i]).append(" ");
         }
         return sb.toString().trim();
+    }
+}
+
+
+/*
+ * @lc app=leetcode.cn id=165 lang=java
+ *
+ * [165] 比较版本号
+ *
+ * https://leetcode.cn/problems/compare-version-numbers/description/
+ *
+ * algorithms
+ * Medium (51.92%)
+ * Likes:    393
+ * Dislikes: 0
+ * Total Accepted:    160.1K
+ * Total Submissions: 308.4K
+ * Testcase Example:  '"1.01"\n"1.001"'
+ *
+ * 给你两个版本号 version1 和 version2 ，请你比较它们。
+ *
+ * 版本号由一个或多个修订号组成，各修订号由一个 '.' 连接。每个修订号由 多位数字 组成，可能包含 前导零。
+ * 每个版本号至少包含一个字符。修订号从左到右编号，下标从 0 开始，最左边的修订号下标为 0 ，下一个修订号下标为 1 ，以此类推。
+ * 例如，2.5.33和 0.1 都是有效的版本号。
+ * 比较版本号时，请按从左到右的顺序依次比较它们的修订号。比较修订号时，只需比较 忽略任何前导零后的整数值 。也就是说，修订号 1 和修订号 001 相等 。
+ * 如果版本号没有指定某个下标处的修订号，则该修订号视为 0 。例如，版本 1.0 小于版本 1.1 ，因为它们下标为 0 的修订号相同，而下标为 1
+ * 的修订号分别为 0 和 1 ，0 < 1 。
+ *
+ * 返回规则如下：
+ * 如果 version1 > version2 返回 1，
+ * 如果 version1 < version2 返回 -1，
+ * 除此之外返回 0。
+ *
+ * 示例 1：
+ * 输入：version1 = "1.01", version2 = "1.001"
+ * 输出：0
+ * 解释：忽略前导零，"01" 和 "001" 都表示相同的整数 "1"
+ *
+ * 示例 2：
+ * 输入：version1 = "1.0", version2 = "1.0.0"
+ * 输出：0
+ * 解释：version1 没有指定下标为 2 的修订号，即视为 "0"
+ *
+ * 示例 3：
+ * 输入：version1 = "0.1", version2 = "1.1"
+ * 输出：-1
+ * 解释：version1 中下标为 0 的修订号是 "0"，version2 中下标为 0 的修订号是 "1" 。0 < 1，所以 version1 < version2
+ *
+ * 提示：
+ * 1 <= version1.length, version2.length <= 500
+ * version1 和 version2 仅包含数字和 '.'
+ * version1 和 version2 都是 有效版本号
+ * version1 和 version2 的所有修订号都可以存储在 32 位整数 中
+ */
+class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        // 分割版本号字符串，使用点号作为分隔符
+        String[] parts1 = version1.split("\\.");
+        String[] parts2 = version2.split("\\.");
+
+        int length = Math.max(parts1.length, parts2.length);
+
+        // 遍历每一位版本号
+        for (int i = 0; i < length; i++) {
+            // 将未定义的部分视为 0
+            int num1 = (i < parts1.length) ? Integer.parseInt(parts1[i]) : 0;
+            int num2 = (i < parts2.length) ? Integer.parseInt(parts2[i]) : 0;
+
+            if (num1 > num2) {
+                return 1;  // version1 大于 version2
+            } else if (num1 < num2) {
+                return -1; // version1 小于 version2
+            }
+        }
+
+        return 0;  // 两个版本号相等
     }
 }
