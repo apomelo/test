0a06546f786a32e620090c213919eb6674918077
adream <listentonatural@gmail.com>
2023-10-31 09:37:10 +0800

    msg: algo binary tree

--- src/main/java/test/algorithm/leetcode/AlgoBinaryTree.java
+++ src/main/java/test/algorithm/leetcode/AlgoBinaryTree.java
@@ -2,10 +2,7 @@ package test.algorithm.leetcode;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import test.algorithm.leetcode.AlgoBinaryTree.Node;
 
@@ -129,6 +126,8 @@ public class AlgoBinaryTree {
         log.info("BinaryTreePostorderTraversal: {}", new BinaryTreePostorderTraversal().postorderTraversal(BinaryTreePostorderTraversal.example1()));
         log.info("BinaryTreePostorderTraversal: {}", new BinaryTreePostorderTraversal().postorderTraversal(BinaryTreePostorderTraversal.example2()));
         log.info("BinaryTreePostorderTraversal: {}", new BinaryTreePostorderTraversal().postorderTraversal(BinaryTreePostorderTraversal.example3()));
+        // [173] 二叉搜索树迭代器
+        BSTIterator.example1();
     }
 
 
@@ -2150,5 +2149,129 @@ class BinaryTreePostorderTraversal {
     }
     public static TreeNode example3() {
         return new TreeNode(1);
+    }
+}
+
+
+/*
+ * @lc app=leetcode.cn id=173 lang=java
+ *
+ * [173] 二叉搜索树迭代器
+ *
+ * https://leetcode.cn/problems/binary-search-tree-iterator/description/
+ *
+ * algorithms
+ * Medium (81.65%)
+ * Likes:    718
+ * Dislikes: 0
+ * Total Accepted:    122.3K
+ * Total Submissions: 149.8K
+ * Testcase Example:  '["BSTIterator","next","next","hasNext","next","hasNext","next","hasNext","next","hasNext"]\n' +
+  '[[[7,3,15,null,null,9,20]],[],[],[],[],[],[],[],[],[]]'
+ *
+ * 实现一个二叉搜索树迭代器类BSTIterator ，表示一个按中序遍历二叉搜索树（BST）的迭代器：
+ * BSTIterator(TreeNode root) 初始化 BSTIterator 类的一个对象。BST 的根节点 root 会作为构造函数的一部分给出。
+ * 指针应初始化为一个不存在于 BST 中的数字，且该数字小于 BST 中的任何元素。
+ * boolean hasNext() 如果向指针右侧遍历存在数字，则返回 true ；否则返回 false 。
+ * int next()将指针向右移动，然后返回指针处的数字。
+ *
+ * 注意，指针初始化为一个不存在于 BST 中的数字，所以对 next() 的首次调用将返回 BST 中的最小元素。
+ * 你可以假设 next() 调用总是有效的，也就是说，当调用 next() 时，BST 的中序遍历中至少存在一个下一个数字。
+ *
+ * 示例：
+ * 输入
+ * ["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next",
+ * "hasNext", "next", "hasNext"]
+ * [[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+ * 输出
+ * [null, 3, 7, true, 9, true, 15, true, 20, false]
+ *
+ * 解释
+ * BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+ * bSTIterator.next();    // 返回 3
+ * bSTIterator.next();    // 返回 7
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 9
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 15
+ * bSTIterator.hasNext(); // 返回 True
+ * bSTIterator.next();    // 返回 20
+ * bSTIterator.hasNext(); // 返回 False
+ *
+ * 提示：
+ * 树中节点的数目在范围 [1, 10^5] 内
+ * 0
+ * 最多调用 10^5 次 hasNext 和 next 操作
+ *
+ * 进阶：
+ * 你可以设计一个满足下述条件的解决方案吗？next() 和 hasNext() 操作均摊时间复杂度为 O(1) ，并使用 O(h) 内存。其中 h 是树的高度。
+ */
+@Slf4j
+class BSTIterator {
+    // 解法1
+    private Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<>();
+        // 将根节点和所有左子树节点入栈
+        pushAllLeft(root);
+    }
+
+    public int next() {
+        TreeNode node = stack.pop(); // 弹出当前节点
+        pushAllLeft(node.right); // 将右子树的左子树入栈
+        return node.val;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+
+    // 辅助方法：将节点及其左子树入栈
+    private void pushAllLeft(TreeNode node) {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+    }
+
+    // 解法2
+//    private ArrayDeque<Integer> deque = new ArrayDeque<>();
+//
+//    public BSTIterator(TreeNode root) {
+//        inorderTraversal(root);
+//    }
+//
+//    private void inorderTraversal(TreeNode root) {
+//        if (root == null) {
+//            return;
+//        }
+//
+//        inorderTraversal(root.left);
+//        deque.add(root.val);
+//        inorderTraversal(root.right);
+//    }
+//
+//    public int next() {
+//        return deque.pop();
+//    }
+//
+//    public boolean hasNext() {
+//        return !deque.isEmpty();
+//    }
+
+
+    public static void example1() {
+        TreeNode root = new TreeNode(7, new TreeNode(3), new TreeNode(15, new TreeNode(9), new TreeNode(20)));
+        BSTIterator bSTIterator = new BSTIterator(root);
+        log.info("{}", bSTIterator.next());    // 返回 3
+        log.info("{}", bSTIterator.next());    // 返回 7
+        log.info("{}", bSTIterator.hasNext()); // 返回 True
+        log.info("{}", bSTIterator.next());    // 返回 9
+        log.info("{}", bSTIterator.hasNext()); // 返回 True
+        log.info("{}", bSTIterator.next());    // 返回 15
+        log.info("{}", bSTIterator.hasNext()); // 返回 True
+        log.info("{}", bSTIterator.next());    // 返回 20
+        log.info("{}", bSTIterator.hasNext()); // 返回 False
     }
 }
