d905e1f25b74cd55bbf020e9a454f0f17b6e337c
adream <listentonatural@gmail.com>
2023-11-28 10:01:57 +0800

    msg: test

---
+++ src/main/java/test/java/util/TreeMapTest.java
@@ -0,0 +1,37 @@
+package test.java.util;
+
+import com.alibaba.fastjson.JSON;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.*;
+
+/**
+ * @author C
+ * @date 2023/12/14
+ */
+@Slf4j
+public class TreeMapTest {
+    public static void main(String[] args) {
+        testConstruct();
+    }
+
+
+    /**
+     * 测试 {@link TreeMap#TreeMap(Comparator)} 测试 TreeMap 的构造方法
+     * TreeMap 只能以 key 进行排序，不能以 value 进行排序
+     */
+    public static void testConstruct() {
+        log.info("---------- testConstruct begin ----------");
+        TreeMap<Integer, Integer> treeMap1 = new TreeMap<>();
+        treeMap1.put(1, 1);
+        treeMap1.put(2, 2);
+        treeMap1.put(5, 3);
+        treeMap1.put(3, 2);
+        treeMap1.put(4, 3);
+        log.info("treeMap1: {}", treeMap1);
+        log.info("treeMap1: {}", JSON.toJSONString(treeMap1));
+        log.info("---------- testConstruct end ----------");
+    }
+}
