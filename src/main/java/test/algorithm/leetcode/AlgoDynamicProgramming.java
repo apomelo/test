b8c1c8d17d4eac38449a2cfddd00a9d4710f54c5
adream <listentonatural@gmail.com>
2023-12-27 10:51:50 +0800

    msg: algo

--- src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
+++ src/main/java/test/algorithm/leetcode/AlgoDynamicProgramming.java
@@ -1843,11 +1843,36 @@ class HouseRobberII {
         }
         // 状态转移方程
         int res = Math.max(
+                // 抢，去下下家
                 dp(nums, start + 2, end, memo) + nums[start],
+                // 不抢，去下家
                 dp(nums, start + 1, end, memo)
         );
 
         memo[start] = res;
         return res;
+    }
+
+    /**
+     * 解法2： 官方解法
+     */
+    public int rob2(int[] nums) {
+        int length = nums.length;
+        if (length == 1) {
+            return nums[0];
+        } else if (length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(robRange(nums, 0, length - 2), robRange(nums, 1, length - 1));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int first = nums[start], second = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = second;
+            second = Math.max(first + nums[i], second);
+            first = temp;
+        }
+        return second;
     }
 }
